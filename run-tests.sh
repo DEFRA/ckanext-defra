c174146f5ebea639dc49694c76ab9fcbe9e1da0c
Ross Jones <ross@servercode.co.uk>
2018-06-26 16:35:36 +0100

    Temporarily specify the path for nose

--- run-tests.sh
+++ run-tests.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
-nosetests --nologcapture --with-pylons=test.ini --with-coverage --cover-package=ckanext.defra --cover-inclusive --cover-erase --cover-tests
+
+/usr/lib/ckan/default/bin/nosetests --nologcapture --with-pylons=test.ini --with-coverage --cover-package=ckanext.defra --cover-inclusive --cover-erase --cover-tests ckanext.defra.tests
